13ae28d716a0fb2b4d80eb4720f80567e85288e0
George Ebberson <george.ebberson@gmail.com>
2024-09-14 00:54:56 +0100

    Bring universal dyeing inline with Vanilla Tweaks

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ haphazard ones that we previously had) for the FLS Minecraft server (IYKYK).
 
 Features
 --------
-* **Universal Dyeing.** Whilst Vanilla Tweaks implements universal dyeing, this
-pack builds upon that to include two extra changes: wool can be dyed 1:8 like
-other materials,and concrete can be dyed just like concrete powder.
+* **Universal Dyeing.** Whilst Vanilla Tweaks implements universal dyeing (and,
+  to be clear, this implementation is very heavily based on their version), this
+  pack builds upon that to include two extra changes: wool can be dyed 1:8 like
+  other materials,and concrete can be dyed just like concrete powder.
 * **Honeycomb Storage.** To make honeycomb blocks useful as a storage block,
-breaking them will yield honeycomb rather than honeycomb blocks. Silk Touch
-still works as normal so you can still get honeycomb back if you want.
+  breaking them will yield honeycomb rather than honeycomb blocks. Silk Touch
+  still works as normal so you can still get honeycomb back if you want.
 
 Development
 -----------
@@ -26,5 +27,7 @@ of the word). Should be run on the same version of Minceraft as the server.
 * Redye wool, should work at 1:8 ratio
 * Redye concrete, should work
 * Redye an item to black, should work (checks tagged dyes work)
+* Revoke all advancements and recipes, give yourself 8+ wool and a dye, you
+  should get all wool redye recipes (checks advancements are working)
 * Break a honeycomb block with an unenchanted tool, should get 2-4 honeycomb
 * Break a honeycomb block with silk touch, should get a honeycomb block
--- universal_dyeing.py
+++ universal_dyeing.py
@@ -5,33 +5,35 @@ from pathlib import Path
 
 from output_utils import Inline, write_dict_to_json
 
-OUTPUT_PATH = Path("universal_dyeing", "recipe")  # Relative to `data`
+# All relative to `data`
+BASE_PATH = Path("universal_dyeing")
+RECIPE_PATH = BASE_PATH / "recipe"
+TAGS_PATH = BASE_PATH / "tags" / "item"
+ADVANCEMENT_PATH = BASE_PATH / "advancement" / "recipes" / "building_blocks" / "universal_dyeing"
 
-# List of all the colours to generate items for.
-COLOURS = (
-    "black",
-    "blue",
-    "brown",
-    "cyan",
-    "gray",
-    "green",
-    "light_blue",
-    "light_gray",
-    "lime",
-    "magenta",
-    "orange",
-    "pink",
-    "purple",
-    "red",
-    "white",
-    "yellow",
-)
-
-# Items whose dye should be a universal_dyeing tag rather than the item.
-TAGGED_DYES = ("black", "blue", "brown", "white")
+# All the colours, and any "extra" dyes (on top of <colour>_dye)
+EXTRA_DYES = {
+    "black": ("ink_sac",),
+    "blue": ("lapis_lazuli",),
+    "brown": ("cocoa_beans",),
+    "cyan": None,
+    "gray": None,
+    "green": None,
+    "light_blue": None,
+    "light_gray": None,
+    "lime": None,
+    "magenta": None,
+    "orange": None,
+    "pink": None,
+    "purple": None,
+    "red": None,
+    "white": ("bone_meal",),
+    "yellow": None,
+}
+COLOURS = EXTRA_DYES.keys()
 
 # Blocks to generate for, all will have 8 items + 1 dye for 8 items.
-BLOCKS = (
+ITEMS = (
     "candle",
     "concrete",
     "concrete_powder",
@@ -43,25 +45,15 @@ BLOCKS = (
 )
 
 
-def generate_universal_dyeing(data_folder: Path):
-    """Do the generation."""
-
-    recipe_folder = data_folder / OUTPUT_PATH
+def generate_recipes(data_folder: Path):
+    """Generate all the recipes."""
+    recipe_folder = data_folder / RECIPE_PATH
     recipe_folder.mkdir(parents=True, exist_ok=True)
 
-    for block, colour in itertools.product(BLOCKS, COLOURS):
-
-        # Quite why the VT maintainers didn't make a tag for every colour I have
-        # no idea, they're clearly deranged.
-        dye = (
-            {"tag": f"universal_dyeing:dyes/{colour}_dye"}
-            if colour in TAGGED_DYES else
-            {"item": f"minecraft:{colour}_dye"}
-        )
-
+    for block, colour in itertools.product(ITEMS, COLOURS):
         key = OrderedDict((
             ("#", [Inline({"item": f"minecraft:{col}_{block}"}) for col in COLOURS]),
-            ("O", Inline(dye)),
+            ("O", Inline({"tag": f"universal_dyeing:dyes/{colour}_dye"})),
         ))
 
         result = OrderedDict((
@@ -79,3 +71,65 @@ def generate_universal_dyeing(data_folder: Path):
 
         output_file = recipe_folder / f"{colour}_{block}.json"
         write_dict_to_json(output_file, recipe)
+
+
+def generate_tags(data_folder: Path):
+    """Generate a tag for each colour dye."""
+    tags_folder = data_folder / TAGS_PATH
+    tags_folder.mkdir(parents=True, exist_ok=True)
+
+    dyes_folder = tags_folder / "dyes"
+    dyes_folder.mkdir(parents=True, exist_ok=True)
+
+    for colour in COLOURS:
+        dyes = (f"{colour}_dye",) + (EXTRA_DYES[colour] if EXTRA_DYES[colour] else tuple())
+        tag = OrderedDict((("values", [f"minecraft:{item}" for item in dyes]),))
+        write_dict_to_json(dyes_folder / f"{colour}_dye.json", Inline(tag))
+
+    for item in ITEMS:
+        items = [f"minecraft:{colour}_{item}" for colour in COLOURS]
+        tag = OrderedDict((("values", items),))
+        write_dict_to_json(tags_folder / f"{item}.json", tag)
+
+    named_dyes = [f"{colour}_dye" for colour in COLOURS]
+    other_dyes = sum([list(x) for x in EXTRA_DYES.values() if x], [])
+    all_dyes = named_dyes + other_dyes
+    tag = OrderedDict((("values", [f"minecraft:{item}" for item in all_dyes]),))
+    write_dict_to_json(tags_folder / "dyes.json", tag)
+
+
+def generate_advancements(data_folder: Path):
+    """Generate advancements that "award" the recipes to the players."""
+    advancement_folder = data_folder / ADVANCEMENT_PATH
+    advancement_folder.mkdir(parents=True, exist_ok=True)
+
+    has_dye = OrderedDict((
+        ("trigger", "minecraft:inventory_changed"),
+        ("conditions", Inline({"items": [{"items": "#universal_dyeing:dyes"}]}))
+    ))
+
+    for item in ITEMS:
+        has_block = OrderedDict((
+            ("trigger", "minecraft:inventory_changed"),
+            ("conditions", Inline({"items": [{"items": f"#minecraft:{item}"}]}))
+        ))
+
+        criteria = OrderedDict((
+            ("has_block", has_block),
+            ("has_dye", has_dye),
+        ))
+
+        root = OrderedDict((
+            ("parent", "minecraft:recipes/root"),
+            ("criteria", criteria),
+            ("requirements", Inline([["has_block"], ["has_dye"]])),
+            ("rewards", {"recipes": [f"universal_dyeing:{colour}_{item}" for colour in COLOURS]}),
+        ))
+        write_dict_to_json(advancement_folder / f"{item}.json", root)
+
+
+def generate_universal_dyeing(data_folder: Path):
+    """Do the generation."""
+    generate_recipes(data_folder)
+    generate_tags(data_folder)
+    generate_advancements(data_folder)
